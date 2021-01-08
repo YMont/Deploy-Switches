c716d1b3ed3bb066e3a7031cc5005346c111a7c4
YMont <58437649+YMont@users.noreply.github.com>
2021-01-08 17:32:11 +0800

    Update device_list.py

--- device_list.py
+++ device_list.py
@@ -1,22 +1,15 @@
 def device_list():
     cisco1 = {
-            "host": "192.168.94.251",
-            "username": "dynasafe",
-            "password": "dyna0808",
-            "device_type": "cisco_ios_telnet",
-        #     "port": "23"
+            "host": "192.168.xxx.xxx",
+            "username": "XXX",
+            "password": "XXX",
+            "device_type": "cisco_ios_telnet", # port:23
             }
 #     cisco2 = {
-#             "host": "192.168.45.46",
-#             "username": "yue",
-#             "password": "123",
-#             "device_type": "cisco_ios",
-#             }
-#     cisco3 = {
-#             "host": "192.168.47.49",
-#             "username": "john",
-#             "password": "123",
-#             "device_type": "huawei",
-#             }
+#          "host": "192.168.xxx.xxx",
+#           "username": "XXX",
+#           "password": "XXX",
+#           "device_type": "cisco_ios", # port:22
+#           }
     deviceList = [cisco1]
     return deviceList
